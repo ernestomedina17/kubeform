895c0e3f2eaa4ac65bc3f6384ae1a3e9967087c3
ernestomedina17 <ernestomedina17@github.com>
2020-12-01 21:29:44 -0600

    Adding ec2 instances

---
+++ 2apps/main.tf
@@ -0,0 +1,237 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.aws_region
+  # access_key = "export AWS_ACCESS_KEY_ID='' in .bashrc"
+  # secret_key = "export AWS_SECRET_ACCESS_KEY='' in .bashrc"
+}
+
+# Default VPC
+resource "aws_vpc" "default" {
+  cidr_block = "153.2.0.0/16"
+}
+
+# Internet GW - Default VPC
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.default.id
+}
+
+# Grant Internet access to the Default VPC
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.default.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.default.id
+}
+
+# Subnet
+resource "aws_subnet" "default" {
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = "153.2.0.0/23"
+  map_public_ip_on_launch = true
+}
+
+
+# Firewall - This SGs apply for both App1 & App2 LBs
+resource "aws_security_group" "elb" {
+  name        = "elb-fw"
+  description = "Maps port 80 to 443 for HTTP traffic"
+  vpc_id      = aws_vpc.default.id
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Our default security group to access
+# the instances over SSH and HTTP
+resource "aws_security_group" "default" {
+  name        = "default-fw"
+  description = "EC2 FW"
+  vpc_id      = aws_vpc.default.id
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from the VPC, Port forward
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["153.2.0.0/20"]
+  }
+
+  # HTTPS access from the VPC
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["153.2.0.0/20"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_elb" "app1-elb" {
+  name = "app1-elb"
+
+  subnets         = [aws_subnet.default.id]
+  security_groups = [aws_security_group.elb.id]
+  instances       = [aws_instance.app1-a.id,aws_instance.app1-b.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  listener {
+    instance_port     = 443
+    instance_protocol = "https"
+    lb_port           = 443
+    lb_protocol       = "https"
+  }
+}
+
+resource "aws_elb" "app2-elb" {
+  name = "app2-elb"
+
+  subnets         = [aws_subnet.default.id]
+  security_groups = [aws_security_group.elb.id]
+  instances       = [aws_instance.app2-a.id,aws_instance.app2-b.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  listener {
+    instance_port     = 443
+    instance_protocol = "https"
+    lb_port           = 443
+    lb_protocol       = "https"
+  }
+}
+
+resource "aws_key_pair" "auth" {
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_instance" "app1-a" {
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    host = self.public_ip
+  }
+
+  instance_type = "t2.micro"
+  ami = var.linux_ami_id
+  key_name = aws_key_pair.auth.id
+  vpc_security_group_ids = [aws_security_group.default.id]
+  subnet_id = aws_subnet.default.id
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum check-update",
+      "sudo yum -y install nginx",
+    ]
+  }
+}
+
+resource "aws_instance" "app1-b" {
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    host = self.public_ip
+  }
+
+  instance_type = "t2.micro"
+  ami = var.linux_ami_id
+  key_name = aws_key_pair.auth.id
+  vpc_security_group_ids = [aws_security_group.default.id]
+  subnet_id = aws_subnet.default.id
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum check-update",
+      "sudo yum -y install nginx",
+    ]
+  }
+}
+
+resource "aws_instance" "app2-a" {
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    host = self.public_ip
+  }
+
+  instance_type = "t2.micro"
+  ami = var.linux_ami_id
+  key_name = aws_key_pair.auth.id
+  vpc_security_group_ids = [aws_security_group.default.id]
+  subnet_id = aws_subnet.default.id
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum check-update",
+      "sudo yum -y install nginx",
+    ]
+  }
+}
+
+resource "aws_instance" "app2-b" {
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    host = self.public_ip
+  }
+
+  instance_type = "t2.micro"
+  ami = var.linux_ami_id
+  key_name = aws_key_pair.auth.id
+  vpc_security_group_ids = [aws_security_group.default.id]
+  subnet_id = aws_subnet.default.id
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum check-update",
+      "sudo yum -y install nginx",
+    ]
+  }
+}
+
---
+++ 2apps/variables.tf
@@ -0,0 +1,19 @@
+variable "key_name" {
+  description = "Desired name of AWS key pair"
+  default = "svcn26-ssh-pub"
+}
+
+variable "public_key_path" {
+  description = "SSH RSA Pub Key"
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-east-2"
+}
+
+variable "linux_ami_id" {
+  description = "RHEL7 AMI - https://aws.amazon.com/marketplace/pp/B08KSFB57X"
+  default     = "ami-05fb6f2cb5192cf18"
+}
