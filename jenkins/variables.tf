be5e3c5945784e5f0291acb8ed82acd2620b9521
ernestomedina17 <ernestomedina17@gmail.com>
2021-02-11 20:59:20 -0600

    create jenkins AMI with packer, manual VPC

--- jenkins/variables.tf
+++ jenkins/variables.tf
@@ -18,7 +18,9 @@ variable "aws_region" {
   default     = "us-east-2"
 }
 
+# List AWS managed AMIs - https://us-east-2.console.aws.amazon.com/imagebuilder/home?region=us-east-2#/images 
+# https://us-east-2.console.aws.amazon.com/systems-manager/parameters/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id/description?region=us-east-2#
 variable "linux_ami_id" {
-  description = "Custom AMI created with Packer and based on RHEL7 - https://aws.amazon.com/marketplace/pp/B08KSFB57X"
-  default     = "ami-0ad900ff53030830f"
+  description = "Custom AMI created with Packer based on ami-09c93f5e8e4b50e05 /aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+  default     = "ami-09c93f5e8e4b50e05"
 }
