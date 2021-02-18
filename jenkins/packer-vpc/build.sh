2ddebaf1e56172e6a23826cdaaa0ecfd2a4e7e57
ernestomedina17 <ernestomedina17@gmail.com>
2021-02-18 12:25:57 -0600

    passing vars to packer

--- jenkins/packer-vpc/build.sh
+++ jenkins/packer-vpc/build.sh
@@ -6,9 +6,9 @@ terraform apply -auto-approve
 packer validate jenkins-ami.json
 
 packer build \
- -var vpc_id=$(terraform output vpc_id) \
- -var subnet_id=$(terraform output subnet_id) \
- -var security_group_id=$(terraform output security_group_id) \
+ -var vpc_id=$(terraform output -raw vpc_id) \
+ -var subnet_id=$(terraform output -raw subnet_id) \
+ -var security_group_id=$(terraform output -raw security_group_id) \
  jenkins-ami.json
 
 terraform destroy -auto-approve
