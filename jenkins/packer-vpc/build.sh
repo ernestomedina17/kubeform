67feea1fe38bda628e5d503130cf254bd649f022
ernestomedina17 <ernestomedina17@gmail.com>
2021-03-16 17:07:03 -0600

    adding comments

--- jenkins/packer-vpc/build.sh
+++ jenkins/packer-vpc/build.sh
@@ -3,16 +3,23 @@
 # Get latest ID
 aws ssm get-parameters-by-path --path /aws/service/ami-amazon-linux-latest | egrep -i 'value|date|arn'
 
+# install ansible roles
+ansible-galaxy install ernestomedina17.java
+ansible-galaxy install ernestomedina17.jenkins
+
+# terraform - aws vpc
 terraform init
 terraform plan
 terraform apply -auto-approve
 packer validate jenkins-ami.json
 
-packer build -force \
+# packer aws ami 
+time packer build -force \
  -var vpc_id=$(terraform output -raw vpc_id) \
  -var subnet_id=$(terraform output -raw subnet_id) \
  -var security_group_id=$(terraform output -raw security_group_id) \
  jenkins-ami-v2.json
 
+# clean up
 terraform destroy -auto-approve
 
