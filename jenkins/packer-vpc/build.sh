ed66eb0dce420b3d9ef0d064d66cffc75925ec99
ernestomedina17 <ernestomedina17@gmail.com>
2021-03-16 12:45:32 -0600

    updating AMI

--- jenkins/packer-vpc/build.sh
+++ jenkins/packer-vpc/build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Get latest ID
+aws ssm get-parameters-by-path --path /aws/service/ami-amazon-linux-latest | egrep -i 'value|date|arn'
+
 terraform init
 terraform plan
 terraform apply -auto-approve
 packer validate jenkins-ami.json
 
-packer build \
+packer build -force \
  -var vpc_id=$(terraform output -raw vpc_id) \
  -var subnet_id=$(terraform output -raw subnet_id) \
  -var security_group_id=$(terraform output -raw security_group_id) \
- jenkins-ami.json
+ jenkins-ami-v2.json
 
 terraform destroy -auto-approve
 
